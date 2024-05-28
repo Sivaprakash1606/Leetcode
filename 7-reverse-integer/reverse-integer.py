8c8d4e6d0f2fec52be5616ea159b5d7e37424806
Sivaprakash1606 <135036499+Sivaprakash1606@users.noreply.github.com>
2024-05-28 08:10:07 +0530

    Time: 33 ms (75.90%) | Memory: 16.4 MB (95.31%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -1,17 +1,15 @@
 class Solution:
     def reverse(self, x: int) -> int:
         list1 = str(x)[::-1]
-        if list1 == '0':  # If x is 0, return 0
+        if list1 == '0':  
             return 0
         if list1[0] == '0':
             list1 = list1[1:]
-        negative = False
         if list1[-1] == "-":
-            negative = True
             list1 = "-" + list1[:-1]
-        result = int(list1)
-        if negative and result < -2**31:
+            if int(list1) < -2**31:
+                return 0
+        if int(list1) >= 2**31:
             return 0
-        elif not negative and result >= 2**31:
-            return 0
-        return result
+        return int(list1)    
+        
