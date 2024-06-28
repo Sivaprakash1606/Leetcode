0ef9ca644d7ddcf286e34eb9acb6830b2bf94548
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-06-28 22:53:29 +0530

    Time: 387 ms (86.72%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,4 +1,9 @@
-SELECT DISTINCT v.author_id AS id
-FROM Views AS v
-WHERE v.author_id = v.viewer_id
-ORDER BY v.author_id ASC;
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id =viewer_id
+ORDER BY author_id ASC;
+
+-- SELECT DISTINCT v.author_id AS id
+-- FROM Views AS v
+-- WHERE v.author_id = v.viewer_id
+-- ORDER BY v.author_id ASC;
