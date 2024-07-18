02ee006de1ef7a90c79c8884721091a369551d52
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-07-18 21:22:43 +0530

    Time: 4 ms (57.91%) | Memory: 43.2 MB (39.44%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.java
+++ 414-third-maximum-number/third-maximum-number.java
@@ -1,26 +1,52 @@
 class Solution {
     public int thirdMax(int[] nums) {
-        double max1 = Double.NEGATIVE_INFINITY;
-        double max2 = Double.NEGATIVE_INFINITY;
-        double max3 = Double.NEGATIVE_INFINITY;
-        for (int num : nums){
-            if (num>max1){
-                max3=max2;
-                max2=max1;
-                max1=num;
-            }else if(num<max1 && num>max2){
-                max3=max2;
-                max2=num;
-            }else if(num<max2 && num>max3){
-                max3=num;
-            }
-
-        }
-        if (max3 == Double.NEGATIVE_INFINITY) {
-            return (int) max1;
-        } else {
-            return (int) max3;
-        }
+        Integer max1 = null;
+        Integer max2 = null;
+        Integer max3 = null;
         
+        for (Integer num : nums) {
+            if (num.equals(max1) || num.equals(max2) || num.equals(max3)) continue;
+
+            if (max1 == null || num > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (max2 == null || num > max2) {
+                max3 = max2;
+                max2 = num;
+            } else if (max3 == null || num > max3) {
+                max3 = num;
+            }
+        }
+
+        return (max3 == null) ? max1 : max3;
     }
 }
+
+
+// class Solution {
+//     public int thirdMax(int[] nums) {
+//         double max1 = Double.NEGATIVE_INFINITY;
+//         double max2 = Double.NEGATIVE_INFINITY;
+//         double max3 = Double.NEGATIVE_INFINITY;
+//         for (int num : nums){
+//             if (num>max1){
+//                 max3=max2;
+//                 max2=max1;
+//                 max1=num;
+//             }else if(num<max1 && num>max2){
+//                 max3=max2;
+//                 max2=num;
+//             }else if(num<max2 && num>max3){
+//                 max3=num;
+//             }
+
+//         }
+//         if (max3 == Double.NEGATIVE_INFINITY) {
+//             return (int) max1;
+//         } else {
+//             return (int) max3;
+//         }
+        
+//     }
+// }
