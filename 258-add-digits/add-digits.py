d17883f7251eba4520626f12dde5088954204690
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-07-22 21:22:39 +0530

    Time: 44 ms (11.08%) | Memory: 16.6 MB (25.03%) - LeetSync

---
+++ 258-add-digits/add-digits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        string=str(num)
+        if len(string)==1:
+            return num
+        summ=0
+        for i in string:
+            summ=summ+int(i)
+        string=str(summ)
+        return self.addDigits(summ)
+
+
+
+        
