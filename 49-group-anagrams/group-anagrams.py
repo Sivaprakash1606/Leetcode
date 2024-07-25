8aeafeb7be4ff62c05ed739b9a2a7df88b683fd7
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-07-25 18:03:35 +0530

    Time: 1257 ms (5.08%) | Memory: 19.7 MB (72.41%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,13 +1,33 @@
+from typing import List
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        d={}
-        for i in strs:
-            s="".join(sorted(i))
-            if s in d:
-                d[s].append(i)
-            else:
-                d[s]=[i]
-        return d.values()
-
-
+        def custom_sort(word):
+            words = list(word)
+            n = len(words)
+            for i in range(n):
+                for j in range(0, n - i - 1):
+                    if words[j] > words[j + 1]:
+                        words[j], words[j + 1] = words[j + 1], words[j]
+            return ''.join(words)
         
+        anagrams = []
+        sorted_words = []
+        
+        for word in strs:
+            sorted_word = custom_sort(word)
+            added = False
+            
+            for i in range(len(sorted_words)):
+                if sorted_words[i] == sorted_word:
+                    anagrams[i].append(word)
+                    added = True
+                    break
+            
+            if not added:
+                sorted_words.append(sorted_word)
+                anagrams.append([word])
+        
+        return anagrams
+
+
