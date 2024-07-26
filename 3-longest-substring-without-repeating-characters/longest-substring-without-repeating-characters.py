a8c5cfee57fb6fbdb8712e720734b34028e46b8f
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-07-26 19:06:45 +0530

    Time: 57 ms (50.82%) | Memory: 16.6 MB (31.84%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,20 +1,33 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        res=""
-        r=""
-        i=0
-        while i <len(s):
-            if s[i] not in res:
-                res=res+s[i]  
-            elif s[i] in res:
-                if len(res)>len(r):
-                    r=res    
-                res=res[res.index(s[i]) + 1:] + s[i]
-            i=i+1   
-        if len(res)>len(r):
-            return len(res)
-        else:
-            return len(r)     
+        res=0
+        res_set=set()
+        l=0
+        for i in range(len(s)):
+            while s[i] in res_set:
+                res_set.remove(s[l])
+                l=l+1
+            res_set.add(s[i])
+            res=max(res,i-l+1)
+        return res    
+
+
+
+        # res=""
+        # r=""
+        # i=0
+        # while i <len(s):
+        #     if s[i] not in res:
+        #         res=res+s[i]  
+        #     elif s[i] in res:
+        #         if len(res)>len(r):
+        #             r=res    
+        #         res=res[res.index(s[i]) + 1:] + s[i]
+        #     i=i+1   
+        # if len(res)>len(r):
+        #     return len(res)
+        # else:
+        #     return len(r)     
         
 
 
