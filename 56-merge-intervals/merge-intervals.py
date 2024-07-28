e70db1cab69594dcd2d3a768602396b48b349b66
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-07-28 15:47:15 +0530

    Time: 120 ms (73.16%) | Memory: 21.2 MB (21.20%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -1,20 +1,31 @@
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:   
-        result=[]
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]: 
+        interval=[]
         intervals.sort()
-        i=0
-        while i<len(intervals):
-            nextstart=intervals[i][0] 
-            currentend=intervals[i][1] 
-            while i<len(intervals)-1 and currentend>=intervals[i+1][0]:
-                currentend=max(currentend,intervals[i+1][1])
-                i=i+1
-            result.append([nextstart,currentend])    
-            i=i+1
-        return result    
+        n,m=intervals[0]
+        for i in range(1,len(intervals)):
+            if m>=intervals[i][0]:
+                m=max(m,intervals[i][1])
+            else:
+                interval.append([n,m])
+                n,m=intervals[i]
+        interval.append([n,m])        
+        return interval      
 
-
-
+        
+        #Another way
+        # result=[]
+        # intervals.sort()
+        # i=0
+        # while i<len(intervals):
+        #     nextstart=intervals[i][0] 
+        #     currentend=intervals[i][1] 
+        #     while i<len(intervals)-1 and currentend>=intervals[i+1][0]:
+        #         currentend=max(currentend,intervals[i+1][1])
+        #         i=i+1
+        #     result.append([nextstart,currentend])    
+        #     i=i+1
+        # return result    
 
         # mistaken code  
         # n = intervals[0][1]
