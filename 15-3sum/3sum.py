e468dcd1a74ada313a63610abaa7ddf306336e8c
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-07-30 12:25:46 +0530

    Time: 2437 ms (5.01%) | Memory: 20.4 MB (83.69%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,28 +1,47 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # using two pointer 
         nums.sort()
-        n,result=len(nums),[]
+        result=set()
+        n=len(nums)
         for i in range(len(nums)):
-            if i>0 and nums[i]==nums[i-1]:
-                continue
             l=i+1
             r=n-1
             while l<r:
-                summ=nums[i]+nums[l]+nums[r]
-                if summ>0:
+                summ=[nums[i],nums[l],nums[r]]
+                sums=sum(summ)
+                if sums>0:
                     r=r-1
-                elif summ<0:
+                elif sums<0:
                     l=l+1
                 else:
-                    result.append([nums[i],nums[l],nums[r]])   
-                    while l<r and nums[l]==nums[l+1]:
-                        l=l+1
-                    while l<r and nums[r]==nums[r-1]:
-                        r=r-1
+                    result.add(tuple(summ))   
                     l=l+1
                     r=r-1
-        return result                
+        return result
+
+        # using two pointer 
+        # nums.sort()
+        # n,result=len(nums),[]
+        # for i in range(len(nums)):
+        #     if i>0 and nums[i]==nums[i-1]:
+        #         continue
+        #     l=i+1
+        #     r=n-1
+        #     while l<r:
+        #         summ=nums[i]+nums[l]+nums[r]
+        #         if summ>0:
+        #             r=r-1
+        #         elif summ<0:
+        #             l=l+1
+        #         else:
+        #             result.append([nums[i],nums[l],nums[r]])   
+        #             while l<r and nums[l]==nums[l+1]:
+        #                 l=l+1
+        #             while l<r and nums[r]==nums[r-1]:
+        #                 r=r-1
+        #             l=l+1
+        #             r=r-1
+        # return result                
 
 
 
