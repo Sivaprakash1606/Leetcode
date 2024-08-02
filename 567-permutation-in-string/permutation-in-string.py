85ee75e861d9301bf49f00f85188797498ef6533
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-08-02 10:17:06 +0530

    Time: 65 ms (53.11%) | Memory: 16.6 MB (51.56%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,17 +1,64 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1)>len(s2):
-            return False
-        s1_sort=sorted(s1)
-        r=0
-        l=len(s1)
-        while l<=len(s2):
-            master=sorted(s2[r:l])
-            if s1_sort==master:
+        need={}
+        for i in s1:
+            if i in need:
+                need[i]+=1
+            else:
+                need[i]=1
+
+        have={}
+        start,end=0,0       
+        while end<len(s2):
+            if s2[end] not in need:
+                have.clear()
+                end=end+1
+                start=end
+                continue
+            if s2[end] not in have:
+                have[s2[end]]=0
+            have[s2[end]]+=1
+            while have[s2[end]]>need[s2[end]]:
+                have[s2[start]]=have[s2[start]]-1
+                start=start+1
+            window=end-start+1
+            if window ==len(s1):
                 return True
-            r=r+1
-            l=l+1   
-        return False        
+            end=end+1
+        return False                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # Correct using inbuild function and not optimised
+
+        # if len(s1)>len(s2):
+        #     return False
+        # s1_sort=sorted(s1)
+        # r=0
+        # l=len(s1)
+        # while l<=len(s2):
+        #     master=sorted(s2[r:l])
+        #     if s1_sort==master:
+        #         return True
+        #     r=r+1
+        #     l=l+1   
+        # return False        
                
             
 
