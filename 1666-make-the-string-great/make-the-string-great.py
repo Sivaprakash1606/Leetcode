27c308a331c77380fda7f66cbe2c9d9f5877505b
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-08-03 09:38:32 +0530

    Time: 28 ms (96.03%) | Memory: 16.5 MB (23.60%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -0,0 +1,26 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        stack=[]
+        for i in s:
+            if stack and ((i.isupper() and stack[-1]==i.lower()) or (i.islower() and stack[-1]==i.upper())):
+                stack.pop()
+            else:
+                stack.append(i)
+        return "".join(stack)  
+
+
+        #Wrong          
+        # s.reverse()
+        # if len(s)<1:
+        #     for i in range(len(s)):
+        #             if s[i]==Uppercase:
+        #                 s[i].remove() and s[j].remove
+        # return s.reverse()  
+
+
+
+
+
+
+
+        
