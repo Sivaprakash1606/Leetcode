aa1f326916d0779ee25e3533f70646086825dbe5
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-08-03 08:51:38 +0530

    Time: 1061 ms (62.50%) | Memory: 55.1 MB (70.62%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -2,45 +2,24 @@ from typing import List
 
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        # Sort intervals based on their end times
+        if not intervals:
+            return 0
+        
+        # Sort intervals by their end times
         intervals.sort(key=lambda x: x[1])
         
         # Initialize variables
-        end = float('-inf')  # Tracks the end time of the last added interval
-        result = 0  # Count of intervals to remove
+        result = 0
+        curr_end = intervals[0][1]
         
-        # Iterate through the sorted intervals
-        for interval in intervals:
-            # If the current interval's start time is greater than or equal to the end of the last interval
-            if interval[0] >= end:
-                # Update the end to the end time of the current interval
-                end = interval[1]
-            else:
-                # Otherwise, increment the removal count since there's an overlap
+        # Iterate through intervals starting from the second one
+        for i in range(1, len(intervals)):
+            # If the current interval's start time is less than the previous end, it's overlapping
+            if intervals[i][0] < curr_end:
+                # Increment removal count since there's an overlap
                 result += 1
+            else:
+                # Otherwise, update curr_end to the end of the current interval
+                curr_end = intervals[i][1]
         
         return result
-
-
-
-
-
-        # intervals.sort()
-        # result=0
-        # i=0
-        # while i<len(intervals)-1:
-        #     curr_end=intervals[i][1]
-        #     while i+1<len(intervals) and curr_end > intervals[i+1][0]:
-        #         result=result+1
-        #         curr_end=min(curr_end,intervals[i+1][1])
-        #         i=i+1
-        #     i=i+1    
-        # return result        
-
-
-
-
-
-        
-
-        
