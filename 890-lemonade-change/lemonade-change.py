78ab1b685b47525705780cdeb4b33f1ec7e056bb
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-08-15 18:46:20 +0530

    Time: 632 ms (23.64%) | Memory: 21.1 MB (58.06%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,18 +1,48 @@
 class Solution:
-    def lemonadeChange(self,bills:List[int])->bool:
-        fives = 0
-        tens = 0
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        d={5:0,10:0,20:0}
         for i in bills:
-            if i == 5:
-                fives += 1
-            elif i == 10 and fives >= 1:
-                fives -= 1
-                tens += 1
-            elif i==20 and fives >= 1 and tens >= 1:
-                tens -= 1
-                fives -= 1
-            elif i==20 and fives >=3:
-                fives -= 3
+            if i==20:
+                if d[10]>0 and d[5]>0:
+                    d[10]=d[10]-1
+                    d[5]=d[5]-1
+                elif d[5]>2:
+                    d[5]=d[5]-3
+                else:
+                    return False    
+            elif i==10:
+                if d[5]==0:
+                    return False    
+                d[5]=d[5]-1
+                d[10]=d[10]+1
             else:
-                return False 
+                d[5]=d[5]+1
         return True 
+
+
+              # or 
+# class Solution:
+#     def lemonadeChange(self,bills:List[int])->bool:
+#         fives = 0
+#         tens = 0
+#         for i in bills:
+#             if i == 5:
+#                 fives += 1
+#             elif i == 10 and fives >= 1:
+#                 fives -= 1
+#                 tens += 1
+#             elif i==20 and fives >= 1 and tens >= 1:
+#                 tens -= 1
+#                 fives -= 1
+#             elif i==20 and fives >=3:
+#                 fives -= 3
+#             else:
+#                 return False 
+#         return True                
+
+
+                    
+
+
+
+        
