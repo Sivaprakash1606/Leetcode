5dce0419a8e11948acd851b9d07673a65e14d170
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-08-20 20:15:50 +0530

    Time: 85 ms (32.13%) | Memory: 16.8 MB (60.32%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.py
@@ -1,12 +1,33 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums1.extend(nums2)
-        nums1.sort()
-        mid=len(nums1)//2
-        if len(nums1)%2 == 0:
-            return (nums1[mid-1]+nums1[mid])/2
-        else:
-            return nums1[mid]   
+        length = (len(nums1) + len(nums2)) // 2  # Corrected the calculation of length
+        even = (len(nums1) + len(nums2)) % 2 == 0  # Check if the total length is even
+        result = []
+        l1, l2 = 0, 0
+        
+        while l1 < len(nums1) or l2 < len(nums2):
+            if len(result) > length:
+                break
+            if l1 < len(nums1) and (l2 >= len(nums2) or nums1[l1] < nums2[l2]):
+                result.append(nums1[l1])
+                l1 += 1
+            else:
+                result.append(nums2[l2])
+                l2 += 1
+        
+        if even:
+            ans = (result[length] + result[length - 1]) / 2.0  # Calculate the average of the two middle numbers
+            return ans
+        return float(result[length])
+
+
+        # nums1.extend(nums2)
+        # nums1.sort()
+        # mid=len(nums1)//2
+        # if len(nums1)%2 == 0:
+        #     return (nums1[mid-1]+nums1[mid])/2
+        # else:
+        #     return nums1[mid]   
             
 
 
