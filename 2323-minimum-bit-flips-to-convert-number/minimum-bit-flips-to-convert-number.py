73d359cc6e0eca64264f2d211bbeab97d46812b5
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-09-12 09:19:10 +0530

    Time: 25 ms (97.30%) | Memory: 16.4 MB (69.14%) - LeetSync

--- 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
+++ 2323-minimum-bit-flips-to-convert-number/minimum-bit-flips-to-convert-number.py
@@ -4,8 +4,8 @@ class Solution:
         b=self.binary(goal)
         
         max_len=max(len(a),len(b))
-        a=a.zfill(max_len)
-        b=b.zfill(max_len)
+        a=self.zeros(a,max_len)
+        b=self.zeros(b,max_len)    
 
         count=0
         for i in range(max_len):
@@ -19,6 +19,12 @@ class Solution:
             binaryy=str(decimal%2)+binaryy
             decimal=decimal//2
         return binaryy if binaryy !="" else "0"
+
+    def zeros(self,num,max_len):
+        while len(num)<max_len:
+            num="0"+num
+        return num    
+
         
              
         
