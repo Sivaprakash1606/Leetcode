6bd6cc6c3af2b9c44026adf7665228bc036eb9ae
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-09-17 21:39:21 +0530

    Time: 304 ms (37.85%) | Memory: 19.3 MB (17.05%) - LeetSync

--- 455-assign-cookies/assign-cookies.py
+++ 455-assign-cookies/assign-cookies.py
@@ -3,8 +3,10 @@ class Solution:
         g.sort()
         s.sort()
         i=j=0
-        while i<len(g) and j<len(s):
+        while j<len(s):
             if g[i]<=s[j]:
                 i=i+1
             j=j+1
+            if i==len(g):
+                break
         return i        
