a82d74c29f0daa2c6d8b0e6b0a8bf457bfe2069c
Sivaprakash <135036499+Sivaprakash1606@users.noreply.github.com>
2024-09-17 15:33:16 +0530

    Time: 38 ms (29.43%) | Memory: 16.5 MB (30.47%) - LeetSync

--- 292-nim-game/nim-game.py
+++ 292-nim-game/nim-game.py
@@ -1,6 +1,8 @@
 class Solution:
     def canWinNim(self, n: int) -> bool:
-        if n%4==0:
-            return False
-        return True        
+        return n%4 != 0
+        
+        # if n%4==0:
+        #     return False
+        # return True        
         
